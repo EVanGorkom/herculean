06f70a7a396f946e96a58493177520652407c7db
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-08-25 01:09:17 -0500

    Create the first route and webpage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  # get "/home/index" 
+  # This takes you to the webpage ONLY IF you add the extension at the end of the localhost:3000 ex "localhost:3000/home/index"
+
+  root 'home#index'
+  # This however makes the root or homepage of your site. So you don't need the extension
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
